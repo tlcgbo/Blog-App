d4cf57de51bd8715898155042f0d3eef931d7ce4
Igbagbo <iolufiranye@gmail.com>
2024-05-23 19:49:32 +0100

    third commit

---
+++ blog-app/src/firebase-config.js
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD4ZX_9xPffaGlO29AMowkzAfNywy0FVBg",
+  authDomain: "blog-app-5b4b8.firebaseapp.com",
+  projectId: "blog-app-5b4b8",
+  storageBucket: "blog-app-5b4b8.appspot.com",
+  messagingSenderId: "866729888922",
+  appId: "1:866729888922:web:e585db873ac1452522a323"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ blog-app/src/pages/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePost = () => {
+  return (
+    <div>
+        <h2>CreatePost</h2>
+    </div>
+  )
+}
+
+export default CreatePost
