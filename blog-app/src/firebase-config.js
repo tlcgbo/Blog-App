fd49349c9f0fc7f608707457fa28cd0c0bbe7167
Igbagbo <iolufiranye@gmail.com>
2024-05-30 20:03:52 +0100

    .

--- blog-app/src/firebase-config.js
+++ blog-app/src/firebase-config.js
@@ -1,7 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { GoogleAuthProvider } from "firebase/auth/cordova";
+import {getAuth, GoogleAuthProvider} from 'firebase/auth' 
 import { getFirestore } from "firebase/firestore";
 
 
--- blog-app/src/pages/Login.jsx
+++ blog-app/src/pages/Login.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import { auth, provider } from "../firebase-config"
+import { signInWithPopup } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
 
-const Login = () => {
+const Login = ({ setIsAuth }) => {
+
+  let navigate = useNavigate();
+
+  const signInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      localStorage.setItem("isAuth", true);
+      setIsAuth(true)
+      navigate('/')
+    })
+  }
+
   return (
     <div className='loginPage'>
     <p>Sign in with Google</p>
