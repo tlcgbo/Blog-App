d10a3d3d5a6296c1ee1f684575bad1e5e5148a17
Igbagbo <iolufiranye@gmail.com>
2024-07-05 20:06:28 +0100

    ..

--- blog-app/src/pages/Blog.jsx
+++ blog-app/src/pages/Blog.jsx
@@ -1,28 +1,45 @@
 import React, {useEffect, useState} from 'react'
+import { getDocs, collection, deleteDoc, doc } from 'firebase/firestore'
+import { db, auth } from '../firebase-config'
 
-const Blog = () => {
+const Blog = ({ isAuth }) => {
+  const [postLists, setPostList] = useState([])
+
+  const postCollectionRef = collection(db, "posts")
+
+  useEffect(() => {
+    const getPosts = async () => {
+      const data = await getDocs(postCollectionRef)
+      setPostList(data.docs.map((doc) => ({...doc.data(), id:doc.id})))
+    }
+    getPosts();
+  })
+
+  const deletePost = async (id) => {
+    const postDoc = doc(db, "posts", id)
+    await deleteDoc(postDoc)
+  }
+
   return (
     <div className='homePage'>
-    
-   
-      <div className='post'>
-        <div className='postHeader'>
-          <div className='title'>
-            <h1></h1>
+      {postLists.map((post) => {
+        return(
+          <div className='post' key={post.id}>
+          <div className='postHeader'>
+            <div className='title'>
+              <h1>{post.title}</h1>
+            </div>
+            <div className='deletePost'>
+              {isAuth && post.author.id === auth.currentUser.uid && <button onClick={() => {deletePost(post.id)} }> &#128465; </button>}
+            </div>
           </div>
-          <div className='deletePost'>
-
-          </div>
+          <div className='postTextContainer'>{post.postText}</div>
+          <h3>@{post.author?.name}</h3>
         </div>
-        <div className='postTextContainer'></div>
-        <h3></h3>
-      </div>
-        
-
-
-      
+        );
+      })}
     </div>
-  )
+  );
 }
 
 export default Blog
